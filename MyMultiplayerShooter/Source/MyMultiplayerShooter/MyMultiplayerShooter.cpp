f54243bf986606fa5e3ec158dc9e54fd6895d215
Bordunos <Mr.bordunos@mail.ru>
2023-02-20 18:48:52 +0300

    Shoo

---
+++ MyMultiplayerShooter/Source/MyMultiplayerShooter/MyMultiplayerShooter.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyMultiplayerShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyMultiplayerShooter, "MyMultiplayerShooter" );
